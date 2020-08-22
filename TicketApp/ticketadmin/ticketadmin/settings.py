e5c57eb1d2d4265a918237e14875acf9f247308f
mite1111 <67484267+mite1111@users.noreply.github.com>
2020-08-22 18:17:37 +0530

    dummy update
    dummy update

--- TicketApp/ticketadmin/ticketadmin/settings.py
+++ TicketApp/ticketadmin/ticketadmin/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
